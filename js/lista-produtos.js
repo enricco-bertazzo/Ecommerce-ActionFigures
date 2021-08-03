7124f1c44a532f29ba4f39b04a56f08cfceecf36
João Matheus <51762648+JoaoMattheus@users.noreply.github.com>
2021-08-03 15:25:14 -0300

    Add files via upload

---
+++ js/lista-produtos.js
@@ -0,0 +1,63 @@
+const items = [
+	{
+		id: 0,
+        	nome: 'Tony Stark',
+        	img: 'images/tony-stark/tony.jpg',
+        	quantidade: 0
+	},
+	{
+		id: 1,
+                nome: 'Steve Rogers',
+                img: 'images/capitao-america/steve.jpg',
+                quantidade: 0
+	},
+	{
+		id: 2,
+                nome: 'Bruce Banner',
+                img: 'images/bruce-bener/bruce.jpg',
+                quantidade: 0
+	},
+]
+
+
+inicializarLoja = () => {
+	var containerProdutos = document.getElementById('produtos');
+        items.map((val) =>{
+        containerProdutos.innerHTML+= `      
+        	<div class="produto-single"> 
+                <img src="`+val.img+`" />
+                <p> `+val.nome+` </p>
+                <a key="`+val.id+`" href="#"> Adicionar ao carrinho </a>                   
+                </div>
+                    `;
+	})
+}
+
+inicializarLoja();
+
+atualizarCarrinho = () =>  {
+	var containerCarrinho = document.getElementById('carrinho');
+        containerCarrinho.innerHTML = "";
+        items.map((val) =>{
+        if(val.quantidade > 0){
+        	containerCarrinho.innerHTML+= `
+                	<div class="info-single-checkout">   
+                	<p style="float:left">Produto: `+val.nome+`</p>
+                	<p style="float:right">Quantidade: `+val.quantidade+`</p>
+                    	<div style="clear:both"></div>
+                    	</div>
+                 `;
+	}
+})
+}
+
+var links = document.getElementsByTagName('a');
+
+for(var i = 0; i < links.length; i++){
+	links[i].addEventListener("click",function(){
+        	let key = this.getAttribute('key');
+                items[key].quantidade++;
+                atualizarCarrinho();
+                return false;
+	})
+}
